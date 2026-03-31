3fc83360e0d7445f964d15e81fb8235be43daeda
shiva <shivamitabse@gmail.com>
2026-03-31 13:38:09 +0530

    ncc

--- models/db.js
+++ models/db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "shivakumar",
-  database: "mydb",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
